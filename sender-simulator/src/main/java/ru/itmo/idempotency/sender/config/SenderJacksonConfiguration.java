ae3de946478485c44e1315a55f6e1b10a7f1ea1f
DenisStepanidenko <stepanidenko.den@mail.ru>
2026-03-30 10:56:40 +0300

    Реализовать загрузку YAML-конфигурации при старте сервиса

---
+++ sender-simulator/src/main/java/ru/itmo/idempotency/sender/config/SenderJacksonConfiguration.java
@@ -0,0 +1,18 @@
+package ru.itmo.idempotency.sender.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SenderJacksonConfiguration {
+
+    @Bean
+    public ObjectMapper senderObjectMapper() {
+        return new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    }
+}

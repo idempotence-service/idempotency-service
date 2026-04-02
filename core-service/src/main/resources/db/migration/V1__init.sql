e4b54048eed76f5da307b184a360be1131112d8a
Viktor Shamin <v.shamin@corp.mail.ru>
2026-04-02 16:28:54 +0300

    Cделать сохранение idempotency-ключей и состояний обработки

--- core-service/src/main/resources/db/migration/V1__init.sql
+++ core-service/src/main/resources/db/migration/V1__init.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS idempotency (
-                                           global_key VARCHAR(512) PRIMARY KEY,
+    global_key VARCHAR(512) PRIMARY KEY,
     source_uid VARCHAR(255) NOT NULL,
     service_name VARCHAR(255) NOT NULL,
     integration_name VARCHAR(255) NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS idempotency (
     status_description VARCHAR(255),
     create_date TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     update_date TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
-    );
+);
 
 CREATE INDEX IF NOT EXISTS idx_idempotency_status_create_date
     ON idempotency (status, create_date);
@@ -19,8 +19,8 @@ CREATE INDEX IF NOT EXISTS idx_idempotency_status_update_date
     ON idempotency (status, update_date);
 
 CREATE TABLE IF NOT EXISTS kafka_event_outbox (
-                                                  id BIGSERIAL PRIMARY KEY,
-                                                  global_key VARCHAR(512),
+    id BIGSERIAL PRIMARY KEY,
+    global_key VARCHAR(512),
     service_name VARCHAR(255),
     integration_name VARCHAR(255),
     yaml_snapshot JSONB NOT NULL,
@@ -30,14 +30,14 @@ CREATE TABLE IF NOT EXISTS kafka_event_outbox (
     result_description VARCHAR(255),
     create_date TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     update_date TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
-    );
+);
 
 CREATE INDEX IF NOT EXISTS idx_kafka_event_outbox_status_create_date
     ON kafka_event_outbox (status, create_date);
 
 CREATE TABLE IF NOT EXISTS event_audit (
-                                           id BIGSERIAL PRIMARY KEY,
-                                           global_key VARCHAR(512),
+    id BIGSERIAL PRIMARY KEY,
+    global_key VARCHAR(512),
     service_name VARCHAR(255),
     integration_name VARCHAR(255),
     reason VARCHAR(255) NOT NULL,
@@ -45,7 +45,7 @@ CREATE TABLE IF NOT EXISTS event_audit (
     payload JSONB,
     yaml_snapshot JSONB,
     create_date TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
-    );
+);
 
 CREATE INDEX IF NOT EXISTS idx_event_audit_create_date
     ON event_audit (create_date);
